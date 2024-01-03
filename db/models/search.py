5fabbda7735dd0c548b670c38007cb2d43f8c2fd
Hazem Elsayed <hazemm.els@gmail.com>
2024-01-03 11:14:13 +0300

    added default values

--- db/models/search.py
+++ db/models/search.py
@@ -1,14 +1,15 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class HadithSearch(BaseModel):
 	"""
 	Search for hadiths
 	"""
-	book: Optional[str]
+	book: Optional[str] = None
 	"""Query string for title of the book of hadith in which the hadith was reported"""
 
-	text_en: Optional[str]
+	text_en: Optional[str] = None
 	"""Query string of the hadith text in English"""
 
-	top_narrator: Optional[str]
+	top_narrator: Optional[str] = None
 	"""Query string of the name of the top most narrator in the sanad chain of the hadith"""
