74b4b0c25ac53c9bb01ffdc526eb92cd5d157ddf
Hazem Elsayed <hazemm.els@gmail.com>
2024-01-03 21:53:08 +0300

    updated auth

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import HTTPBearer
 from fastapi import Depends, HTTPException, status
 import os
 from dotenv import load_dotenv
@@ -7,11 +7,13 @@ load_dotenv()
 
 CLIENT_KEY = os.getenv("CLIENT_KEY")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")  # use token authentication
+bearer_auth = HTTPBearer()  # use token authentication
 
-def validate_token(api_key: str = Depends(oauth2_scheme)):
-    if api_key != CLIENT_KEY:
+def validate_token(token: str = Depends(bearer_auth)):
+    if token.credentials != CLIENT_KEY:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Forbidden"
+            detail="Forbidden",
+            headers={"WWW-Authenticate": "Bearer"},
         )
+    return token.credentials
