0655a754cf9544fe3cbbf1dfc553e2c52f888afb
Hazem Elsayed <hazemm.els@gmail.com>
2024-01-05 09:12:04 +0300

    fixed semanticSearch

--- app/routers/semantic_search/router.py
+++ app/routers/semantic_search/router.py
@@ -11,16 +11,17 @@ router = APIRouter(
 	dependencies=[Depends(validate_token)]
 )
 
-@router.api_route('/{path:path}', methods=['POST'])
+@router.api_route('{path:path}', methods=['POST'])
 async def forward(request: Request, path: str):
 	method = request.method
 	body = await request.body()
 	headers = request.headers
+	params = request.query_params
 
 	url = os.getenv('SEMANTIC_SEARCH_URL')
 	url = url + "/" + path
 
 	async with httpx.AsyncClient() as client:
-		response = await client.request(method, url, data=body, headers=headers)
+		response = await client.request(method, url, data=body, params=params)
 
 	return Response(content=response.content, status_code=response.status_code, headers=response.headers)
